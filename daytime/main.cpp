ff8c305edea40c31682f64c0f6ea6764be2af263
Anna-19 <akalgudaeva@yandex.ru>
2023-11-13 18:50:04 +0300

    3

---
+++ daytime/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+    int server_socket = socket(AF_INET, SOCK_DGRAM, 0); // создаем UDP-сокет
+
+    if (server_socket == -1) {
+        std::cerr << "Ошибка при создании сокета" << std::endl;
+        return 1;
+    }
+
+    sockaddr_in server_address;
+    std::memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(44214); // например, используем порт 12345 для службы daytime
+
+    if (bind(server_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        std::cerr << "Ошибка при привязке сокета" << std::endl;
+        close(server_socket);
+        return 1;
+    }
+
+    std::cout << "Сервер службы daytime запущен" << std::endl;
+
+    char buffer[1024];
+    sockaddr_in client_address;
+    socklen_t client_address_len = sizeof(client_address);
+    int bytes_received = recvfrom(server_socket, buffer, sizeof(buffer), 0, (sockaddr*)&client_address, &client_address_len);
+
+    if (bytes_received == -1) {
+        std::cerr << "Ошибка при получении данных" << std::endl;
+    } else {
+        time_t raw_time;
+        time(&raw_time);
+        std::string time_str = ctime(&raw_time);
+        sendto(server_socket, time_str.c_str(), time_str.size(), 0, (sockaddr*)&client_address, client_address_len);
+        std::cout << "Отправлено клиенту: " << time_str << std::endl;
+    }
+
+    close(server_socket);
+
+    return 0;
+}
---
+++ echo/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0); // создаем TCP-сокет
+
+    if (server_socket == -1) {
+        std::cerr << "Ошибка при создании сокета" << std::endl;
+        return 1;
+    }
+
+    sockaddr_in server_address;
+    std::memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(7777); 
+
+    if (bind(server_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        std::cerr << "Ошибка при привязке сокета" << std::endl;
+        close(server_socket);
+        return 1;
+    }
+
+    if (listen(server_socket, 5) == -1) {
+        std::cerr << "Ошибка при прослушивании сокета" << std::endl;
+        close(server_socket);
+        return 1;
+    }
+
+    std::cout << "Сервер службы echo запущен" << std::endl;
+
+    sockaddr_in client_address;
+    socklen_t client_address_len = sizeof(client_address);
+    int client_socket = accept(server_socket, (sockaddr*)&client_address, &client_address_len); // принимаем входящее соединение
+
+    if (client_socket == -1) {
+        std::cerr << "Ошибка при принятии соединения" << std::endl;
+        close(server_socket);
+        return 1;
+    }
+
+    char buffer[1024];
+    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+
+    if (bytes_received == -1) {
+        std::cerr << "Ошибка при получении данных" << std::endl;
+    } else {
+        send(client_socket, buffer, bytes_received, 0);
+        std::cout << "Получено и отправлено: " << buffer << std::endl;
+    }
+
+    close(client_socket);
+    close(server_socket);
+
+    return 0;
+}
+
